f43d1e856d0996a2f2e2e8c7911f1e9c71bdb9e3
[NIKESH] <71482593+Nikeshvarma12@users.noreply.github.com>
2021-07-16 22:08:02 +0530

    righttriangle

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,10 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	x=int((x2-x1)**2)+(y2-y1)**2
+	y=int((x3-x1)**2)+(y3-y1)**2
+	z=int((x3-x2)**2)+(y3-y2)**2
+	if((x>0 and y>0 and z>0) and (x==(y+z) or y == (x+z) or z==(y+z))):
+		return True 
+	else:
+		return False
