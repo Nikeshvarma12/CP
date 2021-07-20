451548891f3a024fd354611db1a2556f7a6eb332
[NIKESH] <71482593+Nikeshvarma12@users.noreply.github.com>
2021-07-20 11:22:16 +0530

    triangle area

---
+++ 09-isperfectsquare-Python/isperfectsquare.py
@@ -0,0 +1,13 @@
+# isPerfectSquare(n) [10pts]
+# Write the function isPerfectSquare(n) that takes a possibly-non-int value, and returns True if
+# it is an int that is a perfect square (that is, if there exists an integer m such that
+# m**2 == n), and False otherwise. Do not crash on non-ints nor on negative ints.
+import math
+def isperfectsquare(n):
+	
+	if(int(n)<0):
+		return False
+	else:
+		if(math.sqrt(n)**2==n):
+			return True
+	return False
