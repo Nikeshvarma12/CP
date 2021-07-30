9dca383c6738c03451597129f247f32c3a597437
[NIKESH] <71482593+Nikeshvarma12@users.noreply.github.com>
2021-07-30 12:32:10 +0530

    rots

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -6,5 +6,24 @@
 # is a rotation of itself.
 
 def isrotation(x, y):
-	# Your code goes here
-	pass
+    # Your code goes here
+    size1 = len(str(x))
+    size2 = len(str(y))
+    temp = ''
+
+    # Check if sizes of two strings are same
+    if size1 != size2:
+        return False
+
+    # Create a temp string with value str1.str1
+    temp = str(x) + str(x)
+
+    # Now check if str2 is a substring of temp
+    # string.count returns the number of occurrences of
+    # the second string in temp
+    if (temp.count(str(y)) > 0):
+        return True
+    elif(str(y)[::-1] == str(x) or str(x)[::-1] == str(y)):
+        return True
+    else:
+        return False
