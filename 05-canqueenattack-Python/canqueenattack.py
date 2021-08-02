dc48f5d3881158ef055e346c2d07bae25739b073
[NIKESH] <71482593+Nikeshvarma12@users.noreply.github.com>
2021-08-02 12:17:13 +0530

    cqa

--- 05-canqueenattack-Python/canqueenattack.py
+++ 05-canqueenattack-Python/canqueenattack.py
@@ -4,5 +4,14 @@
 # diagonally.
 
 def canqueenattack(qR, qC, oR, oC):
-	# Your code goes here
-	pass
+	if qR == oR:
+		return True
+
+	if qC == oC:
+		return True
+
+	if (qR - oR) == (qC - oC):
+		return True
+	
+	else:
+		return False
