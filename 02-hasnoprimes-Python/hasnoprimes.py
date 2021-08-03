b005e299115a2a24bdac28442f157425309154e4
[NIKESH] <71482593+Nikeshvarma12@users.noreply.github.com>
2021-08-03 13:39:11 +0530

    np

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -3,5 +3,23 @@
 
 
 def fun_hasnoprimes(l):
+	n=len(l)
+	for i in range(n):
+		b=len(l[i])
+		for j in range(b):
+			if(isprime(l[i][j])):
+				return False
 	return True
+def isprime(n):
+    if(n<=1):
+        return False
+    if(n==2):
+        return True
+    if(n%2==0):
+        return False
+    maxfactor=round(n**0.5)
+    for i in range(3,maxfactor+1):
+        if(n%i==0):
+            return False
+    return True
 
