7ab1cb19bdf231822473a9fc132fd7a80777ab91
[NIKESH] <71482593+Nikeshvarma12@users.noreply.github.com>
2021-08-04 11:14:56 +0530

    aa

--- areAnagrams.py
+++ areAnagrams.py
@@ -11,7 +11,11 @@
 # Hint: The time complexity can be achieved in Linear.
 
 def areAnagrams(s1, s2):
-    # Your code goes here...
-    pass
-
+    if(sorted(s1) == sorted(s2)):
+        return True
+    else:
+        return False
 # write your test cases here...
+p= areAnagrams("listen","silent")
+print(p)
+
