e1f4c65d723ec5dbd1d6a33cef7c2ef9ea66c525
[NIKESH] <71482593+Nikeshvarma12@users.noreply.github.com>
2021-08-05 23:14:39 +0530

    quiz

--- quiz.py
+++ quiz.py
@@ -5,21 +5,35 @@
 # This list indicates that student0 scored 88 on quiz0, 80 on quiz1, and 91 on quiz2. Also, student1 scored 68 on quiz0, 100 on quiz1, and did not take quiz2. The function returns the quiz with the highest average. In this case, quiz0 average is 78, quiz1 average is 90, and quiz2 average is 91 (since we ignore the -1). Thus, quiz2 is the best, and so the function returns 2 in this case. You are not responsible for malformed input, except you should return None if there are no quizzes. Also, resolve ties in favor of the lower quiz number. Here is a test function for you:
 
 def bestQuiz(l):
-    # Your  code goes ehre...
-    pass
+    currentAverage = 0 
+    bestAverage = 0 
+    numOfScores = 0 
+    bestQuiz = None
+    for c in range(len(l[0])): 
+        currSum = 0
+        numOfScores = 0 
+        for r in range(len(l)):
+            if (l[r][c] != -1):
+                numOfScores += 1
+                currSum += l[r][c] 
+        if numOfScores > 0:
+            currentAverage = currSum//numOfScores
+            if currentAverage > bestAverage: 
+                bestAverage = currentAverage
+                bestQuiz = c
+    return bestQuiz
 
-def testBestQuiz():
-    print('Testing bestQuiz()...', end='')
-    a = [ [ 88,  80, 91 ],
+
+a = [ [ 88,  80, 91 ],
           [ 68, 100, -1 ]]
-    assert(bestQuiz(a) == 2)
-    a = [ [ 88,  80, 80 ],
+assert(bestQuiz(a) == 2)
+a = [ [ 88,  80, 80 ],
           [ 68, 100, 100 ]]
-    assert(bestQuiz(a) == 1)
-    a = [ [88, -1, -1 ],
+assert(bestQuiz(a) == 1)
+a = [ [88, -1, -1 ],
           [68, -1, -1 ]]
-    assert(bestQuiz(a) == 0)
-    a = [ [-1, -1, -1 ],
+assert(bestQuiz(a) == 0)
+a = [ [-1, -1, -1 ],
           [-1, -1, -1 ]]
-    assert(bestQuiz(a) == None)
-    print('All test cases passed...!')
+assert(bestQuiz(a) == None)
+print('All test cases passed...!')
